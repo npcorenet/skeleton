06da3c0e7ad7d9d0334aac4127785c26e9723fcf
namePlayer <r06sollfrank@gmail.com>
2022-09-11 15:00:16 +0200

    Finish Cache Service

--- src/Service/CacheService.php
+++ src/Service/CacheService.php
@@ -9,13 +9,29 @@ use App\Software;
 class CacheService
 {
 
+    public function writeToCache(string $filename, string|array $data, bool $isJson = true): void
+    {
+        $fileLocation = Software::CACHE_DIR . '/' . $filename . '.json';
+        if ($isJson === FALSE) {
+            $fileLocation = Software::CACHE_DIR . '/' . $filename;
+        }
+
+        if($isJson === true && is_array($data))
+        {
+            file_put_contents($fileLocation, json_encode($data));
+            return;
+        }
+
+        file_put_contents($fileLocation, $data);
+    }
+
     /**
      * @throws CacheException
      */
     public function loadFromCache(string $filename, bool $isJson = true): array
     {
         $fileLocation = Software::CACHE_DIR . '/' . $filename . '.json';
-        if (!$isJson) {
+        if ($isJson === FALSE) {
             $fileLocation = Software::CACHE_DIR . '/' . $filename;
         }
 
@@ -27,8 +43,22 @@ class CacheService
         return json_decode($cacheContents, true);
     }
 
-    public function writeToCache(string $filename)
+    /**
+     * @throws CacheException
+     */
+    public function deleteFromCache(string $filename, bool $isJson = true): void
     {
+        $fileLocation = Software::CACHE_DIR . '/' . $filename . '.json';
+        if ($isJson === FALSE) {
+            $fileLocation = Software::CACHE_DIR . '/' . $filename;
+        }
+
+        if(!file_exists($fileLocation))
+        {
+            throw new CacheException('File not in Cache: ' . str_replace(Software::CACHE_DIR . '/', '', $fileLocation));
+        }
+
+        unlink($fileLocation);
     }
 
 }
