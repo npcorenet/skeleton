f98ac922c9b859a181651edb68d880b89a64c90f
namePlayer <r06sollfrank@gmail.com>
2022-09-11 17:19:50 +0200

    Create Logger

--- public/index.php
+++ public/index.php
@@ -4,4 +4,5 @@ require_once __DIR__.'/../vendor/autoload.php';
 \App\Software::initEnvironment();
 
 require_once __DIR__.'/../config/container.php';
+
 require_once __DIR__.'/../config/routes.php';
