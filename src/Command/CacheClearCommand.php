e3c4ba3580eb6b3f54714079d75901054e1ec52e
namePlayer <r06sollfrank@gmail.com>
2024-04-04 20:42:15 +0200

    Update Packages

--- src/Command/CacheClearCommand.php
+++ src/Command/CacheClearCommand.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Software;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'cache:clear',
+    description: 'Clears the Applications Cache'
+)]
 class CacheClearCommand extends Command
 {
 
-    protected static $defaultName = 'cache:clear';
-    protected static $defaultDescription = 'Clear the application cache';
-
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->write('<fg=black;bg=blue>Starting Cache Clearing Operation</>' . PHP_EOL);
 
